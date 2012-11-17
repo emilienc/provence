050625473e6262b4d853f99b6230ac6d304af4f2
emilien <emilien.charbonnier@free.fr>
2012-11-17 07:19:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Provence::Application.config.secret_token = 'a00f3672421f31971b5ee12214ba70a264926ae7ccb57fbfc686cb2511ac87fdfb21e3416a93177d024f9f2f5d09a46302e2bf15a97f01a1ff1a43384151064a'
