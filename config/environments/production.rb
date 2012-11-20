101a5edc5fd7a809e129d11d07b4a4d1af464a90
emilien <emilien.charbonnier@free.fr>
2012-11-20 07:28:40 +0100

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Provence::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,10 @@ Provence::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( style.css )
+  
+  # Heroku requires this to be false
+  config.assets.initialize_on_precompile=false
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
