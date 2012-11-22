575101f23ee98a6b786c12a7fdba7d283e33c484
emilien <emilien.charbonnier@free.fr>
2012-11-22 18:58:04 +0100

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,8 @@ Provence::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( style.css )
+  #config.assets.precompile += %w( style.css )
+  config.assets.precompile += %w( *.css *.js )
   
   # Heroku requires this to be false
   config.assets.initialize_on_precompile=false
